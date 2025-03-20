f5fae413951994c4898f4f3905183a58cd56772e
iptoux <iptoux@gmail.com>
2025-03-20 08:08:30 +0100

    Add TodoProgressbarComponent to display task progress
    Introduced a progress bar component to reflect task completion percentages, using data from the TasksService. Integrated it into the app layout and ensured unit tests verify its correct creation and functionality.

---
+++ src/app/components/todo-progressbar/todo-progressbar.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {TasksService} from '../../services/tasks.service';
+import {Subscription} from 'rxjs';
+import {Task} from '../../interfaces/task';
+
+@Component({
+  selector: 'app-todo-progressbar',
+  imports: [],
+  templateUrl: './todo-progressbar.component.html',
+  styleUrl: './todo-progressbar.component.css'
+})
+export class TodoProgressbarComponent implements OnInit, OnDestroy {
+  tasks: Task[] = [];
+  private subscription: Subscription | null = null;
+
+  constructor(private tasksService: TasksService) {}
+
+  ngOnInit() {
+    this.subscription = this.tasksService.tasks$.subscribe(tasks => {
+      this.tasks = tasks;
+    });
+  }
+
+  get progress(): number {
+    let tasksCount = this.tasks.length;
+    let tasksCompleted = this.tasks.filter(task => task.completed).length;
+    return (tasksCompleted / tasksCount) * 100;
+  }
+
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+}
+
