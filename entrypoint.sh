b933e6a2877242441c4f1be65cc7661a7264946a
iptoux <donvito@kabelmail.de>
2025-04-05 10:34:27 +0200

    Use `npx` to run Angular CLI in entrypoint script.
    Replaced the direct `ng` command with `npx ng` to ensure compatibility with local Angular CLI installations. Added background execution for the development server to prevent blocking the script flow.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /app/AngularTasks
-ng serve --host 0.0.0.0 --disable-host-check --poll 2000
+#ng serve --host 0.0.0.0 --disable-host-check --poll 2000
+npx ng serve --host 0.0.0.0 --disable-host-check --poll 2000 &
