bfcda263e7befa401ff1cf7d1b65b3b77def43a6
iptoux <donvito@kabelmail.de>
2025-04-08 22:29:13 +0200

    Add AnnouncementBoxComponent to display informational alerts
    Implemented a new AnnouncementBoxComponent with HTML, CSS, and a test spec to display styled informational messages. Includes functionality for dismissible alerts and basic component unit testing.

---
+++ src/app/components/announcement-box/announcement-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-announcement-box',
+  imports: [],
+  templateUrl: './announcement-box.component.html',
+  styleUrl: './announcement-box.component.css'
+})
+export class AnnouncementBoxComponent {
+
+}
