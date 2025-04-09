bf851c587cb81922bd955e1f45e355268089e229
iptoux <donvito@kabelmail.de>
2025-04-09 13:00:18 +0200

    Add dark mode support to TaskAddComponent
    Integrated `DarkModeService` to determine theme state and applied dark mode styles using `NgClass` in the component template. Updated CSS to define styling rules for dark mode, enhancing user experience in low-light environments.

--- src/app/components/task-add/task-add.component.ts
+++ src/app/components/task-add/task-add.component.ts
@@ -2,19 +2,27 @@ import { Component } from '@angular/core';
 import { Task} from '../../interfaces/task';
 import { TasksService} from '../../services/tasks.service';
 import {FormsModule} from '@angular/forms';
+import {DarkModeService} from '../../services/dark-mode.service';
+import {NgClass} from '@angular/common';
 
 
 @Component({
   selector: 'app-task-add',
   imports: [
-    FormsModule
+    FormsModule,
+    NgClass
   ],
   templateUrl: './task-add.component.html',
   styleUrl: './task-add.component.css'
 })
 export class TaskAddComponent {
 
-  constructor(private tasksService: TasksService) {
+  constructor(private tasksService: TasksService,
+              private darkModeService: DarkModeService) {
+  }
+
+  get isDarkMode(): boolean {
+    return this.darkModeService.isDarkMode();
   }
 
   addTask(taskDescription:string) {
