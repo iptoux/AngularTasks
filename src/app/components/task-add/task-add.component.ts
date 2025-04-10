f2ec4c20388dcdb144531fea39d26ac7813461e9
iptoux <donvito@kabelmail.de>
2025-04-11 01:38:56 +0200

    Add error handling for empty task descriptions
    Introduced a validation check to prevent adding tasks with empty descriptions. If the description is empty, an error modal is displayed to the user. Updated the form input to include the 'required' attribute for additional validation.

--- src/app/components/task-add/task-add.component.ts
+++ src/app/components/task-add/task-add.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { Task} from '../../interfaces/task';
 import { TasksService} from '../../services/tasks.service';
 import {FormsModule} from '@angular/forms';
 import {DarkModeService} from '../../services/dark-mode.service';
 import {NgClass} from '@angular/common';
 import {NgbDatepickerModule, NgbTooltip} from '@ng-bootstrap/ng-bootstrap';
+import {ModalService} from '../../services/modal.service';
 
 
 @Component({
@@ -21,17 +22,26 @@ import {NgbDatepickerModule, NgbTooltip} from '@ng-bootstrap/ng-bootstrap';
 export class TaskAddComponent {
   selectedDateTime: string = new Date().toISOString().slice(0, 16); // Format: "YYYY-MM-DDThh:mm"
 
+  modalService = inject(ModalService)
+
   constructor(private tasksService: TasksService,
               private darkModeService: DarkModeService) {
   }
-
-
 
   get isDarkMode(): boolean {
     return this.darkModeService.isDarkMode();
   }
 
   addTask(taskDescription:string) {
+
+    if(taskDescription.length === 0) {
+      // console.log("Empty task description");
+      this.modalService.showErrorModal(
+        "Empty task description",
+        "Please enter a task description")
+      return;
+    }
+
 
     let new_id = this.tasksService.getLastID() + 1;
 
