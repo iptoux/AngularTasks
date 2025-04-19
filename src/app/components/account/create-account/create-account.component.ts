ba3327f026db9c93778abe4325e9a1c3e26fa8cc
iptoux <iptoux@gmail.com>
2025-04-19 22:46:33 +0200

    Remove unused 'inject' import from create-account component.
    The 'inject' import was not being used in the create-account component and has been removed to clean up the code. This improves readability and reduces unnecessary imports.

--- src/app/components/account/create-account/create-account.component.ts
+++ src/app/components/account/create-account/create-account.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {DarkModeService} from '../../../services/dark-mode.service';
 import {NgClass} from '@angular/common';
