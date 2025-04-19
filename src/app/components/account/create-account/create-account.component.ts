d896c2d1606efc2950081f1a398ea664dd6e436b
iptoux <iptoux@gmail.com>
2025-04-19 22:45:33 +0200

    Revamp create-account UI with form and dark mode support
    Replaced placeholder text with a functional UI for account creation, including username, password, and avatar inputs. Integrated dark mode styling through a service and added responsive CSS adjustments. Improved component modularity with updated imports and reactive capabilities.

--- src/app/components/account/create-account/create-account.component.ts
+++ src/app/components/account/create-account/create-account.component.ts
@@ -1,16 +1,29 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {DarkModeService} from '../../../services/dark-mode.service';
+import {NgClass} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-create-account',
-  imports: [],
+  imports: [
+    NgClass,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './create-account.component.html',
   styleUrl: './create-account.component.css'
 })
 export class CreateAccountComponent implements OnInit {
   action:string = '';
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute,
+              private darkModeService: DarkModeService,) {}
+
+  get isDarkMode(): boolean {
+    return this.darkModeService.isDarkMode();
+  }
+
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
